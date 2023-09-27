6d97d4289bb51c917252719b824b6f342e0cd1f0
smaru1111 <marusou20@gmail.com>
2023-09-27 22:50:05 +0900

    add DBのデータを全件取得してくるapi作った

--- api/src/functions/getData.ts
+++ api/src/functions/getData.ts
@@ -1,15 +1,29 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext, input } from "@azure/functions";
+
+const sqlInput = input.generic({
+    // azure sql databaseの時これ
+    type: 'sql',
+    commandtype: 'text',
+    // SQL文ここに書く
+    commandtext: 'select * from dbo.TodoTable',
+
+    // 接続文字列の設定
+    connectionStringSetting: 'SqlConnectionString'
+})
 
 export async function getData(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    return { body: `Hello, ${name}!` };
+    const data = context.extraInputs.get(sqlInput);
+    context.log("😇: ", data)
+    return {
+        status: 200,
+        body: JSON.stringify(data)
+    };
 }
 
 app.http('getData', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
+    extraInputs: [sqlInput],
     handler: getData
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+type Todo = {
+  id: number
+  todoName: string
+  todoStatus: boolean
+}
+
 function App() {
   // fetch api/getData
-  const [data, setData] = useState('')
+  const [todoData, setTodoData] = useState<Todo[]>([])
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
   // fetch api/addData
   const [todoName, setTodoName] = useState('')
   const [todoStatus, setTodoStatus] = useState(false)
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/getData`);
+      const info = await response.json();
+      setTodoData(() => ([...info ]));
+      console.log(todoData);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
-    fetch('/api/getData')
-      .then(res => res.text())
-      .then(data => setData(data))
-  }, [])
+    fetchData();
+  }, []);
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -26,6 +46,9 @@ function App() {
     })
     const result = await res.text()
     console.log(result)
+    fetchData();
+    setTodoName('')
+    setTodoStatus(false)
   }
 
   const handleChangetodoName = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,12 +61,24 @@ function App() {
 
   return (
     <div className="App">
-      fetching:{data}
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChangetodoName} />
-        <input type="checkbox" onChange={handleChangetodoStatus} />
+        <input type="text" onChange={handleChangetodoName} value={todoName}/>
+        <input type="checkbox" onChange={handleChangetodoStatus} checked={todoStatus}/>
         <button type="submit">submit</button>
       </form>
+      <h1>all Data</h1>
+      <ul>
+        {isLoading && <p>...loading</p>}
+        {isError && <p>Error!</p>}
+        {
+          todoData.map((todo) => (
+            <li key={todo.id}>
+              <p>{todo.todoName}</p>
+              <p>{todo.todoStatus}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
