16ff8611fe8ea406c43390e3133f71123e60a3c2
smaru1111 <marusou20@gmail.com>
2023-10-08 00:02:57 +0900

    fix でぷろいこけたので修正5

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-const Item: React.FC<{ name: string | undefined; id?: number }> = ({ name, id }) => {
+const Item: React.FC<{ name: string | undefined; id?: number }> = ({ name }) => {
   return (
     <div className='w-full h-[50px] flex items-center justify-center my-2.5 border border-black rounded-lg bg-gray-100'>
       {name}
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Todo } from '../types/index';
+import { Todo } from '../types/index'
 
 interface TodoItemProps {
   // 追加のpropsも許容する
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [x: string]: any;
-  todo: Todo;
-  onDelete: () => void;
+  [x: string]: any
+  todo: Todo
+  onDelete: () => void
 }
 
-function TodoItem({ todo, onDelete }: TodoItemProps) { 
+function TodoItem({ todo, onDelete }: TodoItemProps) {
   return (
-    <div className="todo-item">
+    <div className='todo-item'>
       <span>{todo.todoName}</span>
       <span>{todo.todoStatus ? 'true' : ''}</span>
       <span>{todo.todoStatus ? '' : 'false'}</span>
       <button onClick={onDelete}>削除</button>
     </div>
-  );
+  )
 }
 
-export default TodoItem;
+export default TodoItem
